a0321b55fcd8b5fefcc6e3cc5013d29e37566140
Ramil Mavlyutov <63664630+stirk1337@users.noreply.github.com>
2021-07-21 16:11:33 +0500

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,22 +1,23 @@
 vowels = ['a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 VOWELS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-def caesar(Word, sdvig):
+def caesar(Word, shift):
     word_list = list(Word)
     for i in range(len(Word)):
         for j in range(len(vowels)):
             if word_list[i] == vowels[j]:
-                secondSdvig = int(sdvig % len(vowels))
-                if secondSdvig + j >= len(vowels):
-                    word_list[i] = vowels[secondSdvig + j - len(vowels)]
+                secondShift = int(shift % len(vowels))
+                if secondShift + j >= len(vowels):
+                    word_list[i] = vowels[secondShift + j - len(vowels)]
                 else:
-                    word_list[i] = vowels[j + secondSdvig]
+                    word_list[i] = vowels[j + secondShift]
                 break
             if word_list[i] == VOWELS[j]:
-                secondSdvig = int(sdvig % len(VOWELS))
-                if secondSdvig + j >= len(VOWELS):
-                    word_list[i] = VOWELS[secondSdvig + j - len(VOWELS)]
+                secondShift = int(shift % len(VOWELS))
+                if secondShift + j >= len(VOWELS):
+                    word_list[i] = VOWELS[secondShift + j - len(VOWELS)]
                 else:
-                    word_list[i] = VOWELS[j + secondSdvig]
+                    word_list[i] = VOWELS[j + secondShift]
                 break
     return ''.join(word_list)
+
